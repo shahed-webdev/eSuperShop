fc0b8d1ad7afda8b6b9998b43731cbbccde3cc9e
muhebollah <muhebollah.diu@gamil.com>
2020-08-29 18:03:36 +0600

    change

---
+++ eSuperShop.Web/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eSuperShop.Web.Controllers
+{
+    [Authorize]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //add brand
+        public IActionResult AddBrand()
+        {
+            return View();
+        }
+    }
+}
