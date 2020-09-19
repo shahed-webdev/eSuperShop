6993f38265e00fa65b628b57117a16bdad0e7be7
Shahed Al Mahmud <shahedalmahmud@outlook.com>
2020-09-19 16:31:14 +0600

    Update Database

---
+++ eSuperShop.Data/Migrations/20200919102745_AddStoreSlug_Banar.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eSuperShop.Data.Migrations
+{
+    public partial class AddStoreSlug_Banar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StoreBanarUrl",
+                table: "Vendor",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StoreSlugUrl",
+                table: "Vendor",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StoreBanarUrl",
+                table: "Vendor");
+
+            migrationBuilder.DropColumn(
+                name: "StoreSlugUrl",
+                table: "Vendor");
+        }
+    }
+}
