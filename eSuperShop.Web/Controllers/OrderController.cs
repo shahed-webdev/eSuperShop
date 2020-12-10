851d7ceafca880e7e6fcc2988447efef1c7688d3
muhebollah <muhebollah.diu@gamil.com>
2020-12-10 17:27:51 +0600

    datatable added

--- eSuperShop.Web/Controllers/OrderController.cs
+++ eSuperShop.Web/Controllers/OrderController.cs
@@ -3,12 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using eSuperShop.BusinessLogic;
+using JqueryDataTables.LoopsIT;
 using Microsoft.AspNetCore.Authorization;
 
 namespace eSuperShop.Web.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly IOrderCore _order;
+
+        public OrderController(IOrderCore order)
+        {
+            _order = order;
+        }
+
+        //***Admin***//
         [Authorize(Roles = "admin, sub-admin")]
         public IActionResult PendingList()
         {
@@ -27,6 +37,14 @@ namespace eSuperShop.Web.Controllers
         public IActionResult DeliveredList()
         {
             return View();
+        }
+
+        //data-table
+         [Authorize(Roles = "admin, sub-admin")]
+        public IActionResult GetOrderListData(DataRequest request)
+        {
+            var response = _order.AdminWiseList(request);
+            return Json(response.Data);
         }
     }
 }
