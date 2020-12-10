1903033e7e503787778384356f034d26cc79e001
Shahed Al Mahmud <shahedalmahmud@outlook.com>
2020-12-10 17:10:39 +0600

    admin wise order list core

--- eSuperShop.BusinessLogic/Order/IOrderCore.cs
+++ eSuperShop.BusinessLogic/Order/IOrderCore.cs
@@ -1,9 +1,12 @@
 ﻿using eSuperShop.Repository;
+using JqueryDataTables.LoopsIT;
 
 namespace eSuperShop.BusinessLogic
 {
     public interface IOrderCore
     {
         DbResponse<int> OrderPlace(OrderPlaceModel model, string customerUserName);
+
+        DbResponse<DataResult<OrderAdminWiseListModel>> AdminWiseList(DataRequest request);
     }
 }
