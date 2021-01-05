fc427bf95260b4a4ab0af78cf84ecc374b777f89
Shahed Al Mahmud <shahedalmahmud@outlook.com>
2021-01-05 17:37:27 +0600

    customer mobile signUp

--- eSuperShop.BusinessLogic/Customer/CustomerCore.cs
+++ eSuperShop.BusinessLogic/Customer/CustomerCore.cs
@@ -1,7 +1,13 @@
 ﻿using AutoMapper;
+using eSuperShop.Data;
 using eSuperShop.Repository;
+using Microsoft.AspNetCore.Identity;
+using OtpNet;
+using Service.SMS;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace eSuperShop.BusinessLogic
 {
@@ -9,11 +15,13 @@ namespace eSuperShop.BusinessLogic
     {
         private readonly IMapper _mapper;
         private readonly IUnitOfWork _db;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public CustomerCore(IMapper mapper, IUnitOfWork db)
+        public CustomerCore(IMapper mapper, IUnitOfWork db, UserManager<IdentityUser> userManager)
         {
             _mapper = mapper;
             _db = db;
+            _userManager = userManager;
         }
 
         public DbResponse Add(CustomerAddModel model)
@@ -78,6 +86,100 @@ namespace eSuperShop.BusinessLogic
             catch (Exception e)
             {
                 return new DbResponse<List<CustomerAddressBookModel>>(false, e.Message);
+            }
+        }
+
+        public DbResponse SendCode(string mobileNumber, int codeValidSecond)
+        {
+            try
+            {
+                if (_db.Vendor.IsExistPhone(mobileNumber)) return new DbResponse(false, "Phone number already exist");
+
+                OtpServiceSingleton.Instance.PhoneNunber = mobileNumber;
+                #region Generate Code
+                var bytes = Base32Encoding.ToBytes("JBSWY3DPEHPK3PXP");
+
+                OtpServiceSingleton.Instance.Totp = new Totp(bytes, codeValidSecond);
+
+                var code = OtpServiceSingleton.Instance.Totp.ComputeTotp(DateTime.UtcNow);
+
+                var textSms = $"Your Verification code is {code}. This code is valid for {codeValidSecond} second";
+                #endregion
+
+                #region SMS Code
+
+                var massageLength = SmsValidator.MassageLength(textSms);
+                var smsCount = SmsValidator.TotalSmsCount(textSms);
+
+                var smsProvider = new SmsProviderBuilder();
+
+                var smsBalance = smsProvider.SmsBalance();
+                if (smsBalance < smsCount) return new DbResponse(false, "No SMS Balance");
+
+                var providerSendId = smsProvider.SendSms(textSms, mobileNumber);
+
+                if (!smsProvider.IsSuccess) return new DbResponse(false, smsProvider.Error);
+
+                #endregion
+
+                return new DbResponse(true, "Success");
+            }
+            catch (Exception e)
+            {
+                return new DbResponse(false, e.Message);
+            }
+        }
+
+        public async Task<DbResponse> MobileSignUpAsync(CustomerMobileSignUpModel model)
+        {
+            try
+            {
+                long timeStepMatched;
+                var verify = OtpServiceSingleton.Instance.Totp.VerifyTotp(model.Code, out timeStepMatched, window: null);
+                if (model.MobileNumber != OtpServiceSingleton.Instance.PhoneNunber) return new DbResponse(false, "Mobile number not match");
+                if (!verify) return new DbResponse(false, "Invalid Code");
+
+
+
+
+
+
+                //Identity Create
+                var user = new IdentityUser { UserName = model.MobileNumber };
+                var password = model.Password;
+                var result = await _userManager.CreateAsync(user, password).ConfigureAwait(false);
+
+                if (!result.Succeeded) return new DbResponse(false, result.Errors.FirstOrDefault()?.Description);
+
+                await _userManager.AddToRoleAsync(user, UserType.Customer.ToString()).ConfigureAwait(false);
+
+
+
+
+
+                #region SMS Code
+
+                var textSms = $"You have registered Successfully , Your Id: {model.MobileNumber}";
+
+                var massageLength = SmsValidator.MassageLength(textSms);
+                var smsCount = SmsValidator.TotalSmsCount(textSms);
+
+                var smsProvider = new SmsProviderBuilder();
+
+                var smsBalance = smsProvider.SmsBalance();
+                if (smsBalance < smsCount) return new DbResponse(false, "No SMS Balance");
+
+                var providerSendId = smsProvider.SendSms(textSms, model.MobileNumber);
+
+                if (!smsProvider.IsSuccess) return new DbResponse(false, smsProvider.Error);
+
+                #endregion
+
+                return new DbResponse(true, "Success");
+            }
+            catch (Exception e)
+            {
+                return new DbResponse(false, e.Message);
             }
         }
     }
--- eSuperShop.BusinessLogic/Customer/ICustomerCore.cs
+++ eSuperShop.BusinessLogic/Customer/ICustomerCore.cs
@@ -1,5 +1,6 @@
 ﻿using eSuperShop.Repository;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace eSuperShop.BusinessLogic
 {
@@ -8,5 +9,15 @@ namespace eSuperShop.BusinessLogic
         DbResponse Add(CustomerAddModel model);
         DbResponse AddressAdd(CustomerAddressBookModel model, string userName);
         DbResponse<List<CustomerAddressBookModel>> AddressList(string userName);
+
+        DbResponse SendCode(string mobileNumber, int codeValidSecond);
+        Task<DbResponse> MobileSignUpAsync(CustomerMobileSignUpModel model);
+    }
+
+    public class CustomerMobileSignUpModel
+    {
+        public string Password { get; set; }
+        public string Code { get; set; }
+        public string MobileNumber { get; set; }
     }
 }
