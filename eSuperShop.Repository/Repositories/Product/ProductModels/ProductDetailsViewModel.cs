e2ec3706d040dbb29c39631099da58e2e2796720
Shahed Al Mahmud <shahedalmahmud@outlook.com>
2021-03-29 14:52:17 +0600

    shipping details

--- eSuperShop.Repository/Repositories/Product/ProductModels/ProductDetailsViewModel.cs
+++ eSuperShop.Repository/Repositories/Product/ProductModels/ProductDetailsViewModel.cs
@@ -34,7 +34,7 @@ namespace eSuperShop.Repository
         public ICollection<FaqProductWiseViewModel> Faqs { get; set; }
         public ICollection<ProductListViewModel> RecommendedProducts { get; set; }
         public ICollection<ProductListViewModel> VendorMoreProducts { get; set; }
-        public ICollection<CatalogShippingCostViewModel> ShippingDetails { get; set; }
+        public CatalogShippingCostViewModel ShippingDetails { get; set; }
 
     }
 }
