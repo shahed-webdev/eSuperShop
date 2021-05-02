8b01a0314a4c554d7954c6f6552f59f88fbe53c0
MUHIBOLLAH <muhebollah.diu@gmail.com>
2021-05-02 13:02:22 +0600

    buy 1 get1 added

---
+++ eSuperShop.Web/Controllers/SellerPromotionsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace eSuperShop.Web.Controllers
+{
+    [Authorize(Roles = "Seller")]
+    public class SellerPromotionsController : Controller
+    {
+        public IActionResult Buy1Get1Free()
+        {
+            return View();
+        }
+    }
+}
